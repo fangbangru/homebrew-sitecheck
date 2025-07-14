7d6af3e449ff2ee6f4b8cb2b7470c19545fa7699
Bangrui Fang <benfang@Bangruis-MacBook-Air.local>
2025-07-14 17:53:38 -0600

    Add Formula sitecheck v0.1.0

---
+++ sitecheck.rb
@@ -0,0 +1,23 @@
+class Sitecheck < Formula
+  desc "Shell-based site performance checker (ping, HTTP code, timing, httping)"
+  homepage "https://github.com/fangbangru/check-site"
+  url "https://github.com/fangbangru/check-site/archive/v0.1.0.tar.gz"
+  sha256 "YOUR_TARBALL_SHA256"
+  license "MIT"
+
+  depends_on "bash"
+  depends_on "curl"
+  depends_on "bc"
+  depends_on "awk"
+  # 建议用户按需安装 httping
+  option "with-httping", "Enable HTTPS latency test via httping"
+  depends_on "httping" => :optional
+
+  def install
+    bin.install "check_site.sh" => "sitecheck"
+  end
+
+  test do
+    assert_match "Usage: sitecheck", shell_output("#{bin}/sitecheck", 1)
+  end
+end
