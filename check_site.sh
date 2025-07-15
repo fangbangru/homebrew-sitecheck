b9e422d389afb6268771c91aa8303013e4940bda
Bangrui Fang <benfang@Bangruis-MacBook-Air.local>
2025-07-15 14:53:19 -0600

    chore: bump version to v0.1.6

--- check_site.sh
+++ check_site.sh
@@ -30,7 +30,7 @@
 
 set -uo pipefail
 
-VERSION="0.1.5"
+VERSION="0.1.6"
 
 # ANSI 颜色
 GREEN='\033[0;32m'; YELLOW='\033[0;33m'; RED='\033[0;31m'; NC='\033[0m'
--- sitecheck.rb
+++ sitecheck.rb
@@ -1,8 +1,8 @@
 class Sitecheck < Formula
   desc "Shell-based site performance checker (ping, HTTP code, timing, httping)"
   homepage "https://github.com/fangbangru/check-site"
-  url "https://codeload.github.com/fangbangru/homebrew-sitecheck/tar.gz/refs/tags/v0.1.5"
-  sha256 "a3a96240dbd0e376a9e4c82d6bae1030c46861656236d7fd32451aab74bd197c"
+  url "https://codeload.github.com/fangbangru/homebrew-sitecheck/tar.gz/refs/tags/v0.1.6"
+  sha256 "d5558cd419c8d46bdc958064cb97f963d1ea793866414c025906ec15033512ed"
   license "MIT"
 
   depends_on "bash"
