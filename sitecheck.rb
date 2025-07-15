a4114ee4993ce5938464f37059b9704bbad70714
Bangrui Fang <benfang@Bangruis-MacBook-Air.local>
2025-07-14 18:19:42 -0600

    Fix URL to use codeload for v0.1.1 tarball

--- sitecheck.rb
+++ sitecheck.rb
@@ -1,7 +1,7 @@
 class Sitecheck < Formula
   desc "Shell-based site performance checker (ping, HTTP code, timing, httping)"
   homepage "https://github.com/fangbangru/check-site"
-  https://github.com/fangbangru/homebrew-sitecheck/archive/v0.1.1.tar.gz
+  url "https://codeload.github.com/fangbangru/homebrew-sitecheck/tar.gz/refs/tags/v0.1.1"
   sha256 "b0e258f556e556a952aee616f150442f80da8afa0b592307de898900c37b6d14"
   license "MIT"
 
